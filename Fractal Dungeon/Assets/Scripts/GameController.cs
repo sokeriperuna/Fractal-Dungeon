bfcdbfbea27cdd1f9ea86819714accfbf89b3efd
Esko Evtyukov (Scoren Pelikeskus GGJ Next) <eskoonwheels@gmail.com>
2018-07-09 15:39:48 +0300

    Started work on rooms

--- Fractal Dungeon/Assets/Scripts/GameController.cs
+++ Fractal Dungeon/Assets/Scripts/GameController.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct PlayerRefs
+{
+    public Transform    transform;
+    public PlayerEntity entity;
+}
+
+[RequireComponent(typeof(RoomManager))]
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Camera           mainCamera;
+    private CameraController cameraController;
+    private PlayerRefs       playerRefs;
+    private RoomManager      roomManager;
+
+    public GameObject player;
+
+    private void Awake()
+    {
+        InitializeGame();
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void InitializeGame()
+    {
+        mainCamera           = Camera.main;
+        cameraController     = mainCamera.GetComponent<CameraController>();
+        playerRefs.transform = player.transform;
+        playerRefs.entity    = player.GetComponent<PlayerEntity>();
+    }
+
+    private void StartGame()
+    {
+
+    }
 }
---
+++ Fractal Dungeon/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct RoomRefs
+{
+    public Transform transform;
+    public RoomData data;
+}
+
+public struct RoomData
+{
+    int iteration;
+}
+
+public class RoomManager : MonoBehaviour {
+
+    public GameObject roomPrefab;
+    public GameObject roomLinker;
+
+    List<RoomRefs> existingRooms;
+
+    private void Awake()
+    {
+        existingRooms = new List<RoomRefs>();
+    }
+
+
+    private void SpawnNewRoom()
+    {
+        GameObject tempObject = null;
+        if (existingRooms.Count > 0)
+            tempObject = GameObject.Instantiate(roomPrefab, Vector3.zero/*Replace Vector3.zero with a proper value*/, Quaternion.identity, existingRooms[existingRooms.Count - 1].transform);
+        else
+            tempObject = GameObject.Instantiate(roomPrefab, Vector3.zero, Quaternion.identity);
+
+        existingRooms.Add(new RoomRefs());
+        int i = 0;
+        //existingRooms[i];
+    }
+
+    private void IntializeRoom()
+    {
+
+    }
+}
