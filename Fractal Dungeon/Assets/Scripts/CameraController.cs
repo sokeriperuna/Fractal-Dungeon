2a6efa86ecdb9530d5db93c1b1c4b7cdd4b78f4e
ukkoaapro <ukko@aapro.org>
2018-07-10 12:18:12 +0300

    Fixed (hopefully) all of the issues

--- Fractal Dungeon/Assets/Scripts/CameraController.cs
+++ Fractal Dungeon/Assets/Scripts/CameraController.cs
@@ -1,30 +1,16 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD:Fractal Dungeon/Assets/Scripts/PlayerController.cs
-public class PlayerController : MonoBehaviour
-{
-=======
 public class CameraController : MonoBehaviour {
->>>>>>> esko:Fractal Dungeon/Assets/Scripts/CameraController.cs
 
-    private PlayerEntity player;
-
-    private void Awake()
-    {
-        player = GetComponent<PlayerEntity>();
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 input = GetInput();
-        if (input != Vector2.zero)
-            player.MovePlayer(input);
-    }
-
-    private Vector2 GetInput()
-    {
-        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-    }
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
