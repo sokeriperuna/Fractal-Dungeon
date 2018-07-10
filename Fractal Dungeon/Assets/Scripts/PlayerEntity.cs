b21774713f65e9b600665713a2a6a40984412750
Esko Evtyukov (Scoren Pelikeskus GGJ Next) <eskoonwheels@gmail.com>
2018-07-10 12:10:29 +0300

    Working on fractal room generation

--- Fractal Dungeon/Assets/Scripts/PlayerEntity.cs
+++ Fractal Dungeon/Assets/Scripts/PlayerEntity.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class PlayerEntity : MonoBehaviour {
 
-    private int roomIteration;
+
 }
--- Fractal Dungeon/Assets/Scripts/RoomManager.cs
+++ Fractal Dungeon/Assets/Scripts/RoomManager.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct RoomRefs
+class RoomRefs
 {
     public Transform transform;
-    public RoomData data;
+    public RoomData  data;
 }
 
 public struct RoomData
 {
-    int iteration;
+    public int iteration;
+    public double scale;
 }
 
 public class RoomManager : MonoBehaviour {
 
     public GameObject roomPrefab;
     public GameObject roomLinker;
+
+    public float roomSize;
+
+    public Transform rooms;
 
     List<RoomRefs> existingRooms;
 
@@ -25,19 +30,41 @@ public class RoomManager : MonoBehaviour {
         existingRooms = new List<RoomRefs>();
     }
 
-
-    private void SpawnNewRoom()
+    private void Start()
     {
+        for (int i = -1; i < 9; i++)
+            SpawnNewRoom(i);
+                
+
+    }
+
+    private void SpawnNewRoom(int lastIteration)
+    {
+        if (lastIteration < -1 || lastIteration >= existingRooms.Count)
+            Debug.LogError("Incorrect lastIteration: " + lastIteration.ToString());
+            
+
         GameObject tempObject = null;
-        if (existingRooms.Count > 0)
-            tempObject = GameObject.Instantiate(roomPrefab, Vector3.zero/*Replace Vector3.zero with a proper value*/, Quaternion.identity, existingRooms[existingRooms.Count - 1].transform);
+        if (lastIteration < 0)
+            tempObject = GameObject.Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, rooms);
         else
-            tempObject = GameObject.Instantiate(roomPrefab, Vector3.zero, Quaternion.identity);
+            tempObject = GameObject.Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, existingRooms[lastIteration].transform);
 
         existingRooms.Add(new RoomRefs());
-        int i = 0;
-        //existingRooms[i];
+        existingRooms[lastIteration + 1].transform      = tempObject.transform;
+        existingRooms[lastIteration + 1].data.iteration = lastIteration + 1;
+        if (lastIteration > -1)
+            existingRooms[lastIteration + 1].data.scale = GetIterationScale(lastIteration + 1);
+        else
+            existingRooms[lastIteration + 1].data.scale = 1F;
+
+        existingRooms[lastIteration + 1].transform.localPosition = new Vector3(1.25f, 1.25f, 0f);
+        existingRooms[lastIteration + 1].transform.localScale    = new Vector3(0.5f, 0.5f, 1f);
+
+        Debug.Log("Room Iteration: " + existingRooms[lastIteration + 1].data.iteration.ToString() + " Scale: " + existingRooms[lastIteration + 1].data.scale.ToString());
     }
+
+    public static double GetIterationScale(int iteration) { return (1F / (2F * iteration)); }
 
     private void IntializeRoom()
     {
