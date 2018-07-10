3e6d9b1956f3c27e3745efe463ad804435367c7e
ukkoaapro <ukko@aapro.org>
2018-07-10 15:45:56 +0300

    Made some stuff kinda work

--- Fractal Dungeon/Assets/Scripts/DoorScript.cs
+++ Fractal Dungeon/Assets/Scripts/DoorScript.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour {
 
+    public DoorScript linkedDoor;
 
-}
+    public Transform teleportdestination;
+
+
+    void OnTriggerEnter2D(Collider2D _c)
+    {
+        if (_c.tag == "Player")
+            Debug.Log("Teleport!!");
+            //transform.position = teleportdestination.position;
+    }
+} 
